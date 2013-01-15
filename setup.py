60ef928084b187764676f71019243217407d4830
Charles Law <charles.law@gmail.com>
2013-01-14 23:54:02 -0500

    Update setup.py and and add a MANIFEST to work with PyPI

--- setup.py
+++ setup.py
@@ -2,23 +2,45 @@ from setuptools import setup, find_packages
 
 DESCRIPTION = 'Python-based XML/HTML Compiler'
 
-LONG_DESCRIPTION = None
-try:
-    LONG_DESCRIPTION = open('README.text').read()
-except:
-    pass
+LONG_DESCRIPTION = """
+RapydML
+===========
+
+RapydML is a Pythonic abstraction of XML/HTML, adding more 
+functionality, ability to easily integrate it with any HTML
+templating framework, and create your own subset of XML whose
+rules will be enforced through RapydML compiler.
+
+
+Installation
+------------
+To install RapydML simply use easy_install or pip:
+
+    pip install rapydml
+
+or
+
+    easy_install rapydml
+
+
+License
+-------
+The project is GPLv3, but the output is license free. 
+See http://www.gnu.org/licenses/gpl-faq.html#WhatCaseIsOutputGPL.
+
+"""
 
 setup(name='rapydml',
+      version='0.0.1',
       packages=['rapydml'],
       package_data={'rapydml': ['*.txt', 'lib/*', 'markup/*']},
       author='Alexander Tsepkov',
-      author_email='atsepkov@pyjeon.com',
-      url='http://rapydml.pyjeon.com/',
+      url='http://rapydml.pyjeon.com',
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
       platforms=['any'],
       license='GNU GPL3',
       install_requires=[],
-      scripts=['bin/rapydml'],
+      scripts=['rapydml']
 )
 
